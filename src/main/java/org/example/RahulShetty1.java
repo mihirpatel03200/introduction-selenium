b37257bec56523d50cbf19d28f7023085069df0d
mihir <mihir24680@outlook.com>
2026-05-11 09:49:29 +0530

    update-2

--- src/main/java/org/example/RahulShetty1.java
+++ src/main/java/org/example/RahulShetty1.java
@@ -4,10 +4,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class RahulShetty1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriver driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
         System.out.println(driver.getTitle());
         System.out.println(driver.getCurrentUrl());
@@ -16,6 +20,15 @@ public class RahulShetty1 {
         driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("Test105*");
 
         driver.findElement(By.xpath("//button[@class='submit signInBtn']")).click();
+
+//        Thread.sleep(2000);
+        System.out.println(driver.findElement(By.xpath("//p[@class='error']")).getText());
+
+
+        //ForgetPassowrd
+
+        driver.findElement(By.linkText("Forgot your password?")).click();
+
 
     }
 }
