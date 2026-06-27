361135d0dbb7eaf26c8b86002f0f3ebded792fa6
Mihir Patel <163976907+mihirpatel03200@users.noreply.github.com>
2026-06-27 16:21:53 +0530

    Misscelleanous

--- src/main/java/org/example/Miscelleanous.java
+++ src/main/java/org/example/Miscelleanous.java
@@ -1,15 +1,17 @@
 package org.example;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Miscelleanous {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
